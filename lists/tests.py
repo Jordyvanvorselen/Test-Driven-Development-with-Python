78bf40c66f0706c81b34f53ea63f2243e99b7269
Jordy van Vorselen <jordyn00b@hotmail.com>
2016-06-23 10:14:58 +0000

    Changed lists/test/test_home_page_returns_correct_html to now test the correct html instead of the POST request tested in a later test function.

--- lists/tests.py
+++ lists/tests.py
@@ -15,10 +15,8 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
         response = home_page(request)
-        expected_html = render_to_string('home.html', {'new_item_text':  'A new list item'})
+        expected_html = render_to_string('home.html')
         self.assertEqual(expected_html, response.content.decode())
 
     def test_home_page_can_save_a_POST_request(self):
@@ -29,6 +27,8 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html', {'new_item_text':  'A new list item'})
+        self.assertEqual(expected_html, response.content.decode())
 
 class ItemModelTest(TestCase):
 
